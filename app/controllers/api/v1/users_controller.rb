332c371c91ebbde5ea1accf5ab05a6597ddf4e16
Franklin Crosby <obleo33@gmail.com>
2018-02-24 10:02:57 -0700

    Add logic to user model (still not working)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -25,6 +25,6 @@ class Api::V1::UsersController < Api::V1::BaseController
 	private 
 
 	def user_params 
-		params.require(:user).permit(:id, :name, :email, :password)
+		params.require(:user).permit(:id, :name, :email, :password_hash)
 	end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+	include Bcrypt
+
+  def password=(password_hash)
+  	bybug
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password_hash)
+    BCrypt::Password.new(self.password_digest) == password
+  end
 end
+
+
